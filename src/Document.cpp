9fd3c7ec1a6c319639d3e662f1dd568eec1bbc11
adityaKumar <adisapk893585@gmail.com>
2026-06-27 01:04:56 +0530

    Initial Commit

---
+++ src/Document.cpp
@@ -0,0 +1,19 @@
+#include "../include/Document.h"
+
+Document::Document(){
+
+    id = 0;
+    title = "";
+    content = "";
+}
+
+Document::Document(
+    int id,
+    string title,
+    string content
+){
+
+    this->id = id;
+    this->title = title;
+    this->content = content;
+}
